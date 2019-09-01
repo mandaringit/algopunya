541f44f4b691c2822de3e6aefb3fc905879e6cc6
MANDARIN <compurain@naver.com>
2019-09-01 20:20:49 +0900

    0901 문제풀이

--- SWEXP/D3/2806_N_Queen/2806.py
+++ SWEXP/D3/2806_N_Queen/2806.py
@@ -20,7 +20,7 @@ def get_around(x, y, N):
 
             if not stop:
                 if 0 <= x + dx * mul < N and 0 <= y + dy * mul < N:
-                    arounds.append([x + dx * mul, y + dy * mul])
+                    arounds.append((x + dx * mul, y + dy * mul))
                 else:
                     d[i][2] = True
 
@@ -42,33 +42,48 @@ class Node:
         self.x = x
         self.y = y
         self.depth = depth
-        self.forbidden = [[self.x, self.y]]
+        self.forbidden = {(self.x, self.y)}
+        self.forbidden.update(get_around(x, y, N))
 
 
 def DFS(start, N):
     stack = [start]
-
+    count = 0
     while stack:
         node = stack.pop()
 
-        node.forbidden.extend(get_around(node.x, node.y, N))
-
         i = node.x + 1
 
-        # 밑에줄만 검사
-        for j in range(N):
-            if [i, j] not in node.forbidden:
-                next_node = Node(i, j, node.depth + 1, N)
-                next_node.forbidden.extend(node.forbidden)
-                stack.append(next_node)
+        if node.depth == N - 1:
+            count += 1
+        else:
+            # 밑에줄만 검사
+            for j in range(N):
+                if (i, j) not in node.forbidden:
+                    next_node = Node(i, j, node.depth + 1, N)
+                    next_node.forbidden.update(node.forbidden)
+                    stack.append(next_node)
 
+    return count
+
+
+import timeit
+
+start1 = timeit.default_timer()
 
 T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
 
+    count = 0
+
     for j in range(N):
         start = Node(0, j, 0, N)
 
-        DFS(start, N)
+        count += DFS(start, N)
+    print("#{} {}".format(tc, count))
+
+end = timeit.default_timer()
+
+print(end - start1)
