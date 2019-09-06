6324137c71fcdc7981d9df7af6ec1dee0d244914
MANDARIN <compurain@naver.com>
2019-09-07 01:19:18 +0900

    0906 문제풀이

--- SWEXP/A대비/2105_디저트_카페/2105.py
+++ SWEXP/A대비/2105_디저트_카페/2105.py
@@ -3,30 +3,18 @@ import sys
 sys.stdin = open('sample_input.txt', 'r')
 
 
-# 시작 위치, 지금까지 사용한 방향
-def cafe_tour(i, j, used_directions):
-    # lt lb rt rb  순서로 0 1 2 3
-    d = [(-1, -1), (1, -1), (-1, 1), (1, 1)]
-
-    for direction_idx in range(4):
-        dx, dy = d[direction_idx]
-
-        ni = i + dx
-        nj = j + dy
-
-        # 인덱스 범위 안인가?
-        if 0 <= ni < N and 0 <= nj < N:
-
-
 T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
 
-    caffes = [list(map(int, input().split()))]
+    cafes = [list(map(int, input().split())) for _ in range(N)]
     visited = [[0] * N for _ in range(N)]
+    maxV = 0
+    maxPath = 0
 
     for i in range(N):
         for j in range(N):
-            # 4방향 제외
-            caffe_tour(i, j)
+
+
+    print(maxPath)
--- SWEXP/A대비/5653_줄기세포배양/5653.py
+++ SWEXP/A대비/5653_줄기세포배양/5653.py
@@ -1,35 +1,94 @@
-# import sys
-#
-# sys.stdin = open('input.txt', 'r')
+import sys
+
+sys.stdin = open('sample_input.txt', 'r')
 
 
 class StemCell:
 
-    def __init__(self, hp):
+    def __init__(self, x, y, hp):
+        self.x = x
+        self.y = y
         self.life = hp  # 생명력 기준
         self.hp = hp  # 깎을 생명력
-        self.is_activate = False
-        self.passed_time = 0
-        self.dead = False
+        self.activate = False  # 활성여부
+        self.passed_time = 0  # 생긴 이후 지난 시간
+        self.dead = False  # 사망 여부
 
-    def plus_one_hour(self):  # 한시간 경과 후
-        if not self.dead:
-            self.passed_time += 1  # 시간을 추가하고
+    def reproduce(self):
+        global stems
 
-            if self.is_activate:  # 만약 활성상태였으면
-                self.hp -= 1  # 생명력도 깎고
-                if self.hp == 0:
-                    self.dead = True
+        d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-            # passed_time이 hp시간동안 비활성상태
-            if self.passed_time == self.life:
-                self.is_activate = True
+        for dx, dy in d:
+            nx = self.x + dx
+            ny = self.y + dy
+
+            coord = (nx, ny)
+
+            # 이미 생성된 좌표라면
+            if coord in stems:
+                exist_sc = stems[coord]
+                if exist_sc.passed_time == 0 and exist_sc.life < self.life:
+                    exist_sc.life = self.life
+                    exist_sc.hp = self.life
+
+            else:
+                new_sc = StemCell(nx, ny, self.life)
+                stems[(nx, ny)] = new_sc
 
 
+T = int(input())
 
-#
-# T = int(input())
-#
-# for tc in range(1, T + 1):
-#
+for tc in range(1, T + 1):
+    N, M, K = map(int, input().split())  # 세로 가로 배양시간
 
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
+    stems = {}
+
+    # 초기 줄기세포 저장
+    for i in range(N):
+        for j in range(M):
+            if grid[i][j] > 0:
+                sc = StemCell(i, j, grid[i][j])
+                stems[(i, j)] = sc
+
+
+    # K 회 돌기
+    hour = 0
+    while hour < K:
+        hour += 1
+
+        # 현재 존재하는 줄기세포들의 시간을 1 증가시킨다.
+        for stem_cell in stems.values():
+            stem_cell.passed_time += 1
+
+        # 시간이 증가된 줄기세포들만을 보고 프로세스를 진행한다.
+        this_time_cells = list(stems.values())
+        for stem_cell in this_time_cells:
+
+            # 해당 셀이 활성화 되어 있다면
+            if stem_cell.activate:
+
+                # 첫 한시간동안 번식한다.
+                if stem_cell.passed_time == stem_cell.life + 1:
+                    stem_cell.reproduce()
+
+                # 활성화 된 이후부터 피가 1씩 깎인다.
+                if stem_cell.passed_time > stem_cell.life:
+                    stem_cell.hp -= 1
+
+                    if stem_cell.hp == 0:
+                        stem_cell.dead = True
+                        stem_cell.activate = False
+
+            # 경과 시간이 생명력 수치와 같아질때 활성화 시키기.
+            if stem_cell.passed_time == stem_cell.life:
+                stem_cell.activate = True
+
+    count = 0
+
+    for sc in stems.values():
+        if not sc.dead:
+            count += 1
+    print("#{} {}".format(tc, count))
