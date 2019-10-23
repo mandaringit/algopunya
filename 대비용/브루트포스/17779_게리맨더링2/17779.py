1fa9b8bcd31f41346c1532b233f967155f0f6717
MANDARIN <compurain@naver.com>
2019-10-23 23:43:36 +0900

    1023 수정

--- 대비용/브루트포스/17779_게리맨더링2/17779.py
+++ 대비용/브루트포스/17779_게리맨더링2/17779.py
@@ -5,8 +5,8 @@ sys.stdin = open('input.txt', 'r')
 N = int(input())
 city = [list(map(int, input().split())) for _ in range(N)]
 mindiff = 100 * N * N
-for x in range(1, N - 2):
-    for y in range(0, N - 1):
+for x in range(0, N - 2):
+    for y in range(1, N - 1):
         # d1 => ld length 1 ~ y 까지 가능
         # d2 => rd length 1 ~ N - y 까지 가능
         for d1 in range(1, y + 1):
@@ -38,70 +38,7 @@ for x in range(1, N - 2):
 
                             elif x + d1 <= i < N and y + 1 <= j < N:
                                 areas[3] += city[i][j]
-
-                    # 5구역 x_5 는 x ~ x+홀d1+d2까지. < 수정 필요
-                    if (d1 + d2) % 2 == 0:
-                        print("짝")
-                        middle = (d1 + d2) // 2
-                        k = 0
-                        count = 0
-                        for x5 in range(x, x + d1 + d2 + 1):
-
-                            y_start = y - k
-                            for y5 in range(y_start, y_start + (2 * k) + 1):
-
-                                areas[4] += city[x5][y5]
-                                if 0 <= x5 < x + d1 and 0 <= y5 < y + 1:
-                                    areas[0] -= city[x5][y5]
-                                elif 0 <= x5 < x + d1 and y + 1 <= y5 < N:
-                                    areas[1] -= city[x5][y5]
-                                elif x + d1 <= x5 < N and 0 <= y5 < y + 1:
-                                    areas[2] -= city[x5][y5]
-                                elif x + d1 <= x5 < N and y + 1 <= y5 < N:
-                                    areas[3] -= city[x5][y5]
-                                print(x, y, d1, d2, x5, y5, count, k)
-                            if count < middle - 1:
-                                k += 1
-                            elif count == middle - 1:
-                                continue
-                            else:
-                                k -= 1
-
-                            count += 1
-
-                    else:
-                        print("홀")
-                        middle = (d1 + d2) // 2
-                        k = 0
-                        count = 0
-                        for x5 in range(x, x + d1 + d2 + 1):
-
-                            y_start = y - k
-                            for y5 in range(y_start, y_start + (2 * k) + 1):
-
-                                areas[4] += city[x5][y5]
-                                if 0 <= x5 < x + d1 and 0 <= y5 < y + 1:
-                                    areas[0] -= city[x5][y5]
-                                elif 0 <= x5 < x + d1 and y + 1 <= y5 < N:
-                                    areas[1] -= city[x5][y5]
-                                elif x + d1 <= x5 < N and 0 <= y5 < y + 1:
-                                    areas[2] -= city[x5][y5]
-                                elif x + d1 <= x5 < N and y + 1 <= y5 < N:
-                                    areas[3] -= city[x5][y5]
-
-                                print(x, y, d1, d2, x5, y5, count, k)
-                            if d1 > d2:
-                                if count < middle:
-                                    k += 1
-                                else:
-                                    k -= 1
-                            else:
-                                if count < middle - 1:
-                                    k += 1
-                                else:
-                                    k -= 1
-
-                            count += 1
+                    print(areas)
 
                     # 마지막 비교
                     maxV = max(areas)
