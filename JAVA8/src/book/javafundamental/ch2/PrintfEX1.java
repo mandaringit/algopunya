3436bc4df3926df480a0f269a411052f50dc538b
mandaringit <mandarin.dev@kakao.com>
2020-03-19 16:24:26 +0900

    ~자바정석 54p

---
+++ JAVA8/src/book/javafundamental/ch2/PrintfEX1.java
@@ -0,0 +1,31 @@
+package book.javafundamental.ch2;
+
+public class PrintfEX1 {
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 2;
+        char c = 'A';
+
+        int finger = 10;
+        long big = 100_000_000_000L;
+        long hex = 0xFFFF_FFFF_FFFF_FFFFL;
+
+        int octNum = 010;
+        int hexNum = 0x10;
+        int binNum = 0b10;
+
+        // %n이나 /n이나 동일하나 %n이 더 안전하다네?
+        System.out.printf("b=%d %n", b);
+        System.out.printf("s=%d %n", s);
+        System.out.printf("c=%c, %d %n", c, (int) c);
+        System.out.printf("finger=[%5d] %n", finger);
+        System.out.printf("finger=[%-5d] %n", finger);
+        System.out.printf("finger=[%05d] %n", finger);
+        System.out.printf("big=%d %n", big);
+        System.out.printf("hex=%#x %n", hex);
+        System.out.printf("octNum=%o %d %n", octNum, octNum);
+        System.out.printf("hexNum=%x %d %n", hexNum, octNum);
+        System.out.printf("bigNum=%s %d %n", Integer.toBinaryString(binNum), binNum);
+
+    }
+}
