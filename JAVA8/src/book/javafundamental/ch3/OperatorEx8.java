8116d1626f3905c5413e536ba748428727bfbf40
mandaringit <mandarin.dev@kakao.com>
2020-03-20 16:07:07 +0900

    ~ch05

---
+++ JAVA8/src/book/javafundamental/ch3/OperatorEx8.java
@@ -0,0 +1,15 @@
+package book.javafundamental.ch3;
+
+public class OperatorEx8 {
+    public static void main(String[] args) {
+        int a = 1000000;
+        int b = 2000000;
+
+        long c = a * b;
+        System.out.println(c);
+
+        c = (long) a * b;
+        System.out.println(c);
+
+    }
+}
