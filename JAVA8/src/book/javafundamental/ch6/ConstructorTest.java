5610518389790d556c41e1eba239b278feb67cc2
mandaringit <mandarin.dev@kakao.com>
2020-03-24 23:49:12 +0900

    ch 06 객체지향 프로그래밍 ~

---
+++ JAVA8/src/book/javafundamental/ch6/ConstructorTest.java
@@ -0,0 +1,21 @@
+package book.javafundamental.ch6;
+
+class Data1 {
+    int value;
+}
+
+class Data2 {
+    int value;
+
+    Data2(int x){
+        value = x;
+    }
+
+}
+
+class ConstructorTest {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1();
+//        Data2 d2 = new Data2(); // error
+    }
+}
