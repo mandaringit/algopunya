8f70829fe2af25e497007eb2e02f2d648975428b
mandaringit <mandarin.dev@kakao.com>
2020-07-28 19:27:52 +0900

    js: [BOJ] 11654 아스키코드, 11720 숫자의합

---
+++ javascript/BOJ/11720_숫자의합/11720.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const fileStream = fs.createReadStream('input.txt');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: fileStream,
+  output: process.stdout,
+})
+
+let input = []
+
+rl.on("line", (line) => {
+  // 줄별로 해야할 작업
+  input.push(line)
+})
+  .on("close", () => {
+    // 로직
+    console.log(input[1].split("").map(Number).reduce((acc,curr) => acc + curr ,0))
+    process.exit()
+  })
