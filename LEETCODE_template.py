f9a578933ccf6185a708dbe8afc373370529e102
mandaringit <mandarin.dev@kakao.com>
2020-11-04 15:39:39 +0900

    refactor: 리트코드 템플릿 수정(ts & ###접근 추가)

--- LEETCODE_template.py
+++ LEETCODE_template.py
@@ -12,8 +12,12 @@ if not os.path.isdir(directory):
     with open(f"LEETCODE/{underbar_name}/solution.py", 'w') as f:
         print(f"solution.py 생성 완료")
 
+    with open(f"LEETCODE/{underbar_name}/solution.ts", 'w') as f:
+        print(f"solution.ts 생성 완료")
+
     with open(f"LEETCODE/{underbar_name}/README.md", 'w') as f:
-        f.write(f"# [LEETCODE] {question_name}")
+        f.write(f"# [LEETCODE] {question_name}\n\n")
+        f.write(f"### 접근")
         print("README.md 생성 완료")
 else:
     print("경로가 이미 존재합니다.")
